828c036cff529c50a5a26f328c702d54dba9d3d5
JEROLPOA2 <92282918+JEROLPOA2@users.noreply.github.com>
2025-02-19 21:08:54 -0500

    Routes

--- src/app/services/model.service.ts
+++ src/app/services/model.service.ts
@@ -7,15 +7,18 @@ import { Observable } from 'rxjs';
 })
 export class ModelService {
   // Asegúrate de que esta URL coincida con la ubicación y puerto de tu backend.
-  private apiUrl = 'http://localhost:4321/api/';
-  private timeSeries = 'get-time-series';
-  private imageClassifier = 'get-image-classifier';
-  private textGenerator = 'get-text-generator';
+  // private apiUrl = 'http://localhost:4321/api/';
+  private timeSeries =
+    ' https://ecommerce-back-end-y01i.onrender.com/api/get-time-series';
+  private imageClassifier =
+    ' https://ecommerce-back-end-y01i.onrender.com/api/get-image-classifier';
+  private textGenerator =
+    'https://recomdenderapi.onrender.com/recommendations/by-product-id';
 
   constructor(private http: HttpClient) {}
 
   getTimeSeries(data: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl + this.timeSeries, data);
+    return this.http.post<any>(this.timeSeries, data);
   }
 
   // Nuevo método para enviar la imagen al backend
@@ -25,11 +28,11 @@ export class ModelService {
     for (const [key, value] of (formData as any).entries()) {
       console.log(`${key}:`, value);
     }
-    return this.http.post<any>(this.apiUrl + this.imageClassifier, formData);
+    return this.http.post<any>(this.imageClassifier, formData);
   }
 
   getTextGenerator(data: any): Observable<any> {
     console.log('Enviando texto para generación:', data);
-    return this.http.post<any>(this.apiUrl + this.textGenerator, data);
+    return this.http.post<any>(this.textGenerator, data);
   }
 }
